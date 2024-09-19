354c307acbbe029306737f1d290e65e16b4f98ea
Thilankavishka <thilankavishka25@gmail.com>
2024-09-19 18:32:28 +0530

    tailwindcss config

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+
+const Index = () => {
+  return (
+    <View>
+      <Text className="text-5xl font-bold text-green-700">index</Text>
+    </View>
+  );
+};
+
+export default Index;
